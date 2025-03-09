12af0165d29aa72d93aa4ce4510e6b1b7e4f6189
jeritopro2 <jeronimoa.diazh@gmail.com>
2025-03-09 18:02:23 -0500

    Update main.py
    o hi i play fich thank you

--- main.py
+++ main.py
@@ -92,13 +92,11 @@ twitch_miner.mine(
     [
       Streamer("justryyyy", settings=StreamerSettings(follow_raid=True , claim_drops=True  , watch_streak=True)),
       Streamer("LilaFrost21", settings=StreamerSettings(follow_raid=True , claim_drops=True  , watch_streak=True)),
-      Streamer("laruletaderobux_extra", settings=StreamerSettings(follow_raid=True , claim_drops=True  , watch_streak=True)),
+      Streamer("laruletaderobux_plus", settings=StreamerSettings(follow_raid=True , claim_drops=True  , watch_streak=True)),
       Streamer("arguz11", settings=StreamerSettings(follow_raid=True , claim_drops=True  , watch_streak=True)),
       Streamer("Zintid", settings=StreamerSettings(follow_raid=True , claim_drops=True  , watch_streak=True)),
       Streamer("hamingxd", settings=StreamerSettings(follow_raid=True , claim_drops=True  , watch_streak=True)),
       Streamer("wikk6", settings=StreamerSettings(follow_raid=True , claim_drops=True  , watch_streak=True)),
-      Streamer("laruletaderobuxs", settings=StreamerSettings(follow_raid=True , claim_drops=True  , watch_streak=True)),
-        
     ],                                  # Array of streamers (order = priority)
     followers=False,                    # Automatic download the list of your followers
     followers_order=FollowersOrder.ASC  # Sort the followers list by follow date. ASC or DESC
